25b3bf38cbe62c421956b92cdd6b339899b35714
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-04-25 01:20:52 -0500

    update

--- event-workbook/click/app.js
+++ event-workbook/click/app.js
@@ -1,1 +1,5 @@
 // make the box disapear when the user clicks it
+var redBox = document.getElementById("red-box");
+redBox.addEventListener("click", function(){
+    redBox.style.display = "none";
+});
--- event-workbook/keypress/app.js
+++ event-workbook/keypress/app.js
@@ -3,3 +3,31 @@
 Example of output: You've pressed the "a" key. It's key code is 65.
 
 Wes Bos made this https://keycode.info/ for us to use as an example. */
+
+var body = document.getElementById("body")
+
+var  keyCodeFunction = (event) => {
+    if (event.keyCode == "82"){
+        body.style.backgroundColor = "red"
+    };
+     if (event.keyCode == "87"){
+        body.style.backgroundcolor = "white"
+    };
+    if (event.keyCode == "71"){
+        body.style.backgroundColor= "green"
+    };
+     if (event.keyCode == "89"){
+        body.style.backgroundColor = "yellow"
+    };
+     if (event.keyCode == "80"){
+        body.style.backgroundColor = "purple"
+    };
+     if (event.keyCode == "66"){
+        body.style.backgroundColor = "black"    
+    };
+    
+    alert(event.keyCode);
+    console.log(event);
+    };
+
+window.addEventListener("keydown", keyCodeFunction);
